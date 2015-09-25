c3e5f076d893bc7fbf8b2ab7f5ee841943f2e1f1
Kate McFaul <katemcfaul@gmail.com>
2015-09-25 11:01:42 -0700

    add icons to plant list

--- app/helpers/plants_helper.rb
+++ app/helpers/plants_helper.rb
@@ -1,2 +1,31 @@
 module PlantsHelper
+	def how_many(p)
+		if p.squares.empty?
+			how_many = ""
+		else
+			planted = 0
+			scheduled = 0
+			p.squares.each do |square|
+				square.seeds.each do |seed|
+					if seed.planted
+						planted += 1
+					else
+						scheduled += 1
+					end
+				end
+			end
+			if planted != 0
+				planted = planted.to_s + "❀"
+			else
+				planted = ""
+			end
+			if scheduled != 0
+				scheduled = scheduled.to_s + "◷"
+			else
+				scheduled = ""
+			end
+			how_many = "#{scheduled}#{planted}"
+		end
+		return how_many
+	end
 end
